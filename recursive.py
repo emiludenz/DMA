738ce875860b7d9f7ef2f01346096b4e7fb9bded
emiludenz <emilbh@gmail.com>
2018-09-07 08:46:19 +0200

    Update recursive.py

--- recursive.py
+++ recursive.py
@@ -1,3 +1,10 @@
+"""
+"The power of recursion evidently lies in the possibility of defining an 
+infinite set of objects by a finite statement. In the same manner, 
+an infinite number of computations can be described by a finite 
+recursive program, even if this program contains no explicit repetitions."
+"""
+
 def RecT(n):
     # Gives the number times itself
     if n < 1:
